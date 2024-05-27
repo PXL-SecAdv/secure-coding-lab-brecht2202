69dc0838b03fafc21b7edd7e9352a569c95f2055
brecht2202 <brecht.nouboers@hotmail.be>
2024-05-28 00:13:52 +0200

    Adds changes

--- backend/index.js
+++ backend/index.js
@@ -7,18 +7,36 @@ const cors = require('cors')
 
 const port=3000;
 
+const dbUser = process.env.DB_USER || 'secadv';
+const dbHost = process.env.DB_HOST || 'db';
+const dbDatabase = process.env.DB_DATABASE || 'pxldb';
+const dbPassword = process.env.DB_PASSWORD || 'ilovesecurity';
+const dbPort = process.env.DB_PORT || 5432;
+
 const pool = new pg.Pool({
-    user: 'secadv',
-    host: 'db',
-    database: 'pxldb',
-    password: 'ilovesecurity',
-    port: 5432,
+    user: dbUser,
+    host: dbHost,
+    database: dbDatabase,
+    password: dbPassword,
+    port: dbPort,
     connectionTimeoutMillis: 5000
-})
+});
 
 console.log("Connecting...:")
 
-app.use(cors());
+const whitelist = ['http://localhost:8088']
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(
     bodyParser.urlencoded({
@@ -28,16 +46,27 @@ app.use(
 
 app.get('/authenticate/:username/:password', async (request, response) => {
     const username = request.params.username;
-    const password = request.params.password;
+    // const password = request.params.password;
+    const password = await bcrypt.hash(request.params.password, 12);
 
-    const query = `SELECT * FROM users WHERE user_name='${username}' and password='${password}'`;
-    console.log(query);
-    pool.query(query, (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(200).json(results.rows)});
-      
+    // const query = `SELECT * FROM users WHERE user_name='${username}' and password='${password}'`;
+    // console.log(query);
+    // pool.query(query, (error, results) => {
+    //   if (error) {
+    //     throw error
+    //   }
+    //   response.status(200).json(results.rows)});
+    
+    const query = `SELECT * FROM users WHERE user_name = $1 and password = $2`;
+    const values = [username, password];
+
+    try {
+        const results = await pool.query(query, values);
+        response.status(200).json(results.rows);
+    } catch (error) {
+      console.error(error);
+      response.status(500).json({error: 'Internal Server Error'});
+    }
 });
 
 app.listen(port, () => {
--- db/database.sql
+++ db/database.sql
@@ -8,7 +8,9 @@ BEGIN;
 create table users (id serial primary key, user_name text not null unique, password text not null);
 grant all privileges on table users to secadv;
 
-insert into users (user_name, password) values ('pxl-admin', 'insecureandlovinit') ;
-insert into users (user_name, password) values ('george', 'iwishihadbetteradmins') ;
+-- insert into users (user_name, password) values ('pxl-admin', 'insecureandlovinit') ;
+-- insert into users (user_name, password) values ('george', 'iwishihadbetteradmins') ;
+insert into users (user_name, password) values ('pxl-admin', '$2a$12$M2uu.LOVFaQS0favriLdmu2lfVlzedvR5X8Wh1GCUywVAYt5FTG2i') ;
+insert into users (user_name, password) values ('george', '$2a$12$Oc920J0isyU8OkYUBEGe/eNyttAgv0l8/1EQbDUiY91DLNx.BOOfG') ;
 
 COMMIT;
